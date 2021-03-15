f29b6e8caf09d72cc7c1a6c2ea530257340092b6
Samantha <Sambollano54@gmail.com>
2021-03-14 23:33:11 -0500

    .

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {}
 let acceptingAnswers = true;
@@ -52,6 +54,8 @@ getNewQueston = () => {
         return window.location.assign("/end.html");
     }
     questionCounter++;
+    questionCounterText.innerText = ${questionCounter}/${MAX_QUESTIONS};
+    }
     const questionIndex = Math.floor(math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
